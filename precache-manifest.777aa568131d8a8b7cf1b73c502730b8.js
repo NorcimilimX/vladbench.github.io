7fa9b6e192e7f490c98b1a0349b64a1bdee7d6c9
Vlad <freshmen17@ukr.net>
2020-09-18 16:35:16 +0300

    Updates

---
+++ precache-manifest.777aa568131d8a8b7cf1b73c502730b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb109f5e63b89b12aea837f66bed9cea",
+    "url": "/vladbench.github.io/index.html"
+  },
+  {
+    "revision": "5e0f5fce8ee77aab794d",
+    "url": "/vladbench.github.io/static/css/2.dc17f35a.chunk.css"
+  },
+  {
+    "revision": "2f06193c4adfdbcae72e",
+    "url": "/vladbench.github.io/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5e0f5fce8ee77aab794d",
+    "url": "/vladbench.github.io/static/js/2.455fd1f4.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/vladbench.github.io/static/js/2.455fd1f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f06193c4adfdbcae72e",
+    "url": "/vladbench.github.io/static/js/main.32d194b5.chunk.js"
+  },
+  {
+    "revision": "217539d0f1914c3b29f3",
+    "url": "/vladbench.github.io/static/js/runtime-main.23602631.js"
+  }
+]);
